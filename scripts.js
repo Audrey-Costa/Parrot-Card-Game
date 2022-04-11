81e27e98157c324f03adec394186b2f6add0dabc
Audrey-Costa <audreycosta@nano.ufrj.br>
2022-04-11 05:26:58 -0300

    Version 2 Game Complete

--- scripts.js
+++ scripts.js
@@ -5,34 +5,63 @@ let card1;
 let card2; 
 let arrayCards = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
 let gifArray = [];
+let moves = 0;
+let winCount = 0;
+let time = 0;
+let timeId;
+let seconds = 0;
+let minutes = 9;
+let timeFinal = 0;
 
 //Pergunta ao jogador com quantas cartas ele quer jogar, entre 4 a 14.
 function play(){
     const cards = Number(prompt("Escolha 4, 6, 8, 10, 12 ou 14 cartas para jogar!"));
     if (cards === 4 || cards === 6 || cards === 8 || cards === 10 || cards === 12 || cards === 14){
-        arrayRandom(arrayCards)
-        gifArray = arrayCards.slice(0, cards/2)
-        gifArray = gifArray.concat(gifArray)
-        arrayRandom(gifArray)
-        console.log(gifArray)
+        arrayRandom(arrayCards);
+        gifArray = arrayCards.slice(0, cards/2);
+        gifArray = gifArray.concat(gifArray);
+        arrayRandom(gifArray);
         nextPage();
-        console.log(cards)
         for (let i = 0; i < gifArray.length; i++){
             if (i < cards/2){
-                console.log(i, "1")
                 document.querySelector(".superior").innerHTML += `            <div class="card class${i}" onclick="flip(this)" card="${i}">
                 <img src="Images/front.png" />
-              </div>`
-              console.log(document.querySelector(`.class${i}`).getAttribute("card"))
+              </div>`;
             }else if (i < cards){
-                console.log(i, "2")
                 document.querySelector(".inferior").innerHTML += `            <div class="card class${i}" onclick="flip(this)" card="${i}">
                 <img src="Images/front.png" />
-              </div>`
+              </div>`;
             }
         }
+        timeCount()
     }else{
         play();
+    }
+}
+
+function timeCount(){
+    timeId = setInterval(function(){timeLapse()}, 1000)
+}
+
+function timeLapse(){
+    time++
+        seconds = time%60;
+    if (time%60 ===0 ){
+        minutes++
+    }
+    if ((seconds < 10) && (minutes < 10)){
+        document.querySelector(".timer").innerHTML = `0${minutes}:0${seconds}`;
+        timeFinal = `0${minutes}:0${seconds}`;
+    }else if((seconds >= 10) && (minutes < 10)){
+        document.querySelector(".timer").innerHTML = `0${minutes}:${seconds}`;
+        timeFinal = `0${minutes}:${seconds}`;
+    }else if((seconds < 10) && (minutes >= 10)){
+        document.querySelector(".timer").innerHTML = `${minutes}:0${seconds}`;
+        timeFinal = `${minutes}:0${seconds}`;
+    }
+    else if((seconds >= 10) && (minutes >=10)){
+        document.querySelector(".timer").innerHTML = `${minutes}:${seconds}`;
+        timeFinal = `${minutes}:${seconds}`;
     }
 }
 
@@ -46,20 +75,27 @@ function compair(){
 
 //Remove os contéudos da página inicial
 function nextPage(){
-    document.querySelector(".contents").classList.toggle("noContents")
-    document.querySelector("button").classList.toggle("noContents")
-    document.querySelector(".img1").classList.toggle("noContents")
-    document.querySelector(".img2").classList.toggle("noContents")
-    document.querySelector(".img3").classList.toggle("noContents")
-    document.querySelector(".img4").classList.toggle("noContents")
-    document.querySelector(".img5").classList.toggle("noContents")
-    document.querySelector(".img6").classList.toggle("noContents")
-    document.querySelector(".img7").classList.toggle("noContents")
-    document.querySelector(".restart").classList.toggle("noContents")
-    document.querySelector(".timer").classList.toggle("noContents")
+    document.querySelector("h2").classList.toggle("noContents")
+    document.querySelector(".contents").classList.toggle("noContents");
+    document.querySelector("button").classList.toggle("noContents");
+    document.querySelector(".img1").classList.toggle("noContents");
+    document.querySelector(".img2").classList.toggle("noContents");
+    document.querySelector(".img3").classList.toggle("noContents");
+    document.querySelector(".img4").classList.toggle("noContents");
+    document.querySelector(".img5").classList.toggle("noContents");
+    document.querySelector(".img6").classList.toggle("noContents");
+    document.querySelector(".img7").classList.toggle("noContents");
+    document.querySelector(".quit").classList.toggle("noContents");
+    document.querySelector(".timer").classList.toggle("noContents");
     arrayCards = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
-    document.querySelector(".superior").innerHTML = ``
-    document.querySelector(".inferior").innerHTML = ``
+    document.querySelector(".superior").innerHTML = ``;
+    document.querySelector(".inferior").innerHTML = ``;
+    time = 0;
+    moves = 0;
+    winCount = 0;
+    seconds =0;
+    minutes = 0;
+    clearInterval(timeId)
 }
 
 //A função flip adiciona a classe cardSelect correspondente à carta, e atribui a variavel imgAttribute o elemento img correspondente.
@@ -68,27 +104,30 @@ function flip(card) {
     imgAttribute = document.querySelector(`.cardSelected${cardsClicked+1}`).querySelector("img");
 //Adiciona a classe flipped à div e chama a função gifImg para substituir a fonte da imagem da carta.
     if (!card.classList.contains("flipped")) {
+        moves++
         card.classList.add("flipped");
         setTimeout(function(){cardBack(card)}, 300);
-        cardsClicked++
+        cardsClicked++;
     }
 }
 
 //Substitui a fonte da imagem da carta e atribui o elemento alterado ao card1 e card2, para então chamar a função compares.
 function cardBack(element) {
     imgAttribute.setAttribute("src", `/Images/${gifArray[element.getAttribute("card")]}`);
-    console.log(imgAttribute, element, element.getAttribute("card"));
-    console.log(cardsClicked)
     if (cardsClicked === 1){
+        document.querySelector(".block").classList.remove("noContents")
+        setTimeout(function(){unlock()}, 500);
         card1 = imgAttribute.src;
-        console.log("2")
     }
     if (cardsClicked === 2){
         card2 = imgAttribute.src;
-        console.log("3")
         cardsClicked = 0;
-        compares()
+        compares();
     }
+}
+
+function unlock(){
+    document.querySelector(".block").classList.add("noContents");
 }
 
 //Devolve a fonte da frente da imagem da carta.
@@ -100,28 +139,36 @@ function cardFront(element1, element2) {
 function classClearSelect(){
     document.querySelector(`.cardSelected1`).classList.remove("cardSelected1");
     document.querySelector(`.cardSelected2`).classList.remove("cardSelected2");
-    console.log("OOOOIIII")
 }
 
 function classClearFlip(){
-    document.querySelector(`.flipped`).classList.remove("flipped");
-    document.querySelector(`.flipped`).classList.remove("flipped");
-    console.log("OOOOIIII")
+    document.querySelector(`.cardSelected1`).classList.remove("flipped");
+    document.querySelector(`.cardSelected2`).classList.remove("flipped");
 }
 //Compara card1 e card2 se forem iguais retorna as cartas para baixo. Caso contrário as permanece viradas pra cima. Zera cardsCLicked.
 function compares(){
-    console.log("fui chamada")
-    console.log(card1)
-    console.log(card2)
     if (card1 !== card2){
-        console.log("a condição foi cumprida");
-        setTimeout(function() {cardFront(document.querySelector(`.cardSelected1`).querySelector("img"), document.querySelector(`.cardSelected2`).querySelector("img"))}, 900);
-        setTimeout(function() {classClearSelect()}, 905);
-        setTimeout(function() {classClearFlip()}, 905);
+        document.querySelector(".block").classList.remove("noContents")
+        setTimeout(function(){unlock()}, 1900);
+        setTimeout(function() {cardFront(document.querySelector(`.cardSelected1`).querySelector("img"), document.querySelector(`.cardSelected2`).querySelector("img"))}, 800);
+        setTimeout(function() {classClearFlip()}, 850);
+        setTimeout(function() {classClearSelect()}, 850);
     }else{
+        document.querySelector(".block").classList.remove("noContents")
+        setTimeout(function(){unlock()}, 900);
         classClearSelect();
+        winCount++;
+    }
+    if (winCount === gifArray.length/2){
+        alert(`Você venceu em ${moves} jogadas! \nSeu tempo foi de ${timeFinal}`)
+        let newGame = prompt("Você quer jogar uma nova partida?  (sim/não)")
+        if (newGame === "sim"){
+            nextPage();
+            play();
+        } else if (newGame === "não"){
+            nextPage();
+        }
 
     }
-
 }
 
